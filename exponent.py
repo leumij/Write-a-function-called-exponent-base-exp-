2a2ff220e4a91c728f24ad64e760a8d57a4333e2
leumij <cjimuel74@gmail.com>
2024-01-26 20:23:33 +0800

    exponent

---
+++ exponent.py
@@ -0,0 +1,11 @@
+# Note here exp is a non-negative integer, and the base is an integer.
+
+def exponent(base, exp):
+    num = exp
+    result = 1
+    while num > 0:
+        result = result * base
+        num = num - 1
+    print(base, "raises to the power of", exp, "is: ", result)
+
+exponent(5, 4)
